de8f6a4fea8a351378e702e97ecd90e3cb656b83
Daniel Arenas <danielarenas@lahaus.com>
2024-07-10 15:29:08 -0500

    add loading

--- src/views/Availability/Index.vue
+++ src/views/Availability/Index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="flex flex-col">
+  <div v-if="isLoading">
+    <div class="h-screen flex items-center justify-center">
+      <grid-loader :loading="loading" :color="color" :size="size"></grid-loader>
+    </div>
+  </div>
+  <div class="flex flex-col" v-else>
     <div class="flex flex-row justify-between">
       <h1>Contrato {{this.deal?.serial}}</h1>
       <a class="border p-3 bg-amber-300" href="#/">Ver turnos</a>
@@ -56,14 +61,15 @@ import _ from 'lodash';
 import dealRepository from './../../repositories/deal'
 import turnRepository from './../../repositories/turn'
 import availabilityRepository from './../../repositories/availability'
-import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+import GridLoader from 'vue-spinner/src/GridLoader.vue'
 
 export default {
   name: "Availability",
   components: {
-    PulseLoader
+    GridLoader
   },
   data: () => ({
+    isLoading: true,
     dealId: 1, // allways get the deal with id 1
     deal: null,
     services: [],
@@ -82,7 +88,6 @@ export default {
   watch: {
     current_week(_nV, _oV){
       this.getTurn()
-      this.getAvailability()
     }
   },
   methods: {
@@ -90,27 +95,38 @@ export default {
       await this.getDeal(this.dealId)
     },
     async getDeal(dealId) {
-      let dealResponse = await dealRepository.getDeal(dealId)
+      await dealRepository.getDeal(dealId).then(response => {
 
-      this.deal = dealResponse.data.attributes;
-      this.services = dealResponse.data.attributes?.services;
-      this.workers  = dealResponse.data.attributes?.workers;
-      this.weeks    = dealResponse.data.attributes?.weeks;
+        this.deal     = response.data.attributes;
+        this.services = response.data.attributes?.services;
+        this.workers  = response.data.attributes?.workers;
+        this.weeks    = response.data.attributes?.weeks;
 
-      this.current_week    = this.current_week ?? this.weeks[0];
-      this.current_service = this.services[0].id
+        this.current_week    = this.current_week ?? this.weeks[0];
+        this.current_service = this.services[0].id
+      })
     },
     async getTurn(){
-      this.turns = await turnRepository.getTurn(this.current_week, this.current_service)
+      await turnRepository.getTurn(this.current_week, this.current_service)
+                    .then(response => {
+                      this.turns = response 
+                      this.getAvailability()
+                    })
     },
     async getAvailability(){
-      this.availabilities = await availabilityRepository.getAvailability(this.current_week, this.current_service)
-      this.MarkAvailability()
+      await availabilityRepository.getAvailability(this.current_week, this.current_service)
+                    .then(response => {
+                      this.availabilities = response
+                      this.MarkAvailability()
+                      this.isLoading = false
+                    })
     },
     MarkAvailability(){
       this.availabilities.forEach((availability) => this.availabilitiesChecked.push(`${availability.turn_id}_${availability.worker_id}`))
     },
     check (e) {
+      this.isLoading = true
+
       this.$nextTick(() => {
         let [turnId, workerId] = e.target.id.split("_");
 
@@ -118,7 +134,6 @@ export default {
           availabilityRepository.newAvailability(turnId, workerId)
             .then(response => {
               this.getTurn()
-              this.getAvailability()
             })
         } else {
           let availability = this.availabilities.find(availability => availability.turn_id == turnId && availability.worker_id == workerId);
@@ -128,7 +143,6 @@ export default {
             availabilityRepository.destroyAvailability(availabilityId)
               .then(response => {
                 this.getTurn()
-                this.getAvailability()
               })
           } else {
             console.warn('Availability not found for turnId:', turnId, 'and workerId:', workerId);
